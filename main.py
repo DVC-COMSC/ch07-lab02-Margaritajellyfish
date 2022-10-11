02b1d1980a3606a4256739a8c9313fe7834e5745
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 04:20:00 +0000

    7lab2

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # ******************************
 # Make your Code
 # ******************************
+import random
+list_ = []
+for i in range(10):
+    num = random.randint
+    list_.append(random.randint(0,100))
+print(index(min(list_)))
